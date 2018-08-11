57e5bb7a786679a99cb41cbb0d2effd7b9500660
nakabonne <rodriguez.nak@gmail.com>
2018-08-11 10:48:13 +0900

    fix restart

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -7,5 +7,5 @@ cd /var/www/html/webapp/go
 GOPATH=/var/www/html/webapp/go make build
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
-sudo systemctl stop isubata.golang.service
-sudo systemctl start isubata.golang.service
+sudo systemctl stop systemd.go.service
+sudo systemctl start systemd.go.service
